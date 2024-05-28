d04840910b0968f120ecd509b3df9ed1b04cb13d
Priyanshu Yadav <priyanshu.yadav@ksolves.com>
2024-05-28 19:47:19 +0530

    fix css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
   const [selectedCoin, setSelectedCoin] = useRecoilState(selectedCoinState);
 
   return (
-    <nav className="flex justify-center sm:gap-10 md:gap-28 py-4 border-b-[1px] text-xs  text-[#d31c92]">
-      <button onClick={() => setSelectedCoin('virtual')} className={`px-3 h-5 md:text-sm border-r-[0px] ${selectedCoin === 'virtual' ? 'text-blue-700' : ''}`}>Virtual Coin</button>
-      <button onClick={() => setSelectedCoin('10rs')} className={`px-3 h-5 md:text-sm border-r-[1px]  ${selectedCoin === '10rs' ? 'text-blue-700' : ''}`}>10 Rs Coin</button>
-      <button onClick={() => setSelectedCoin('5rs')} className={`px-3 h-5 md:text-sm border-r-[1px] ${selectedCoin === '5rs' ? 'text-blue-700' : ''}`}>5 Rs Coin</button>
-      <button onClick={() => setSelectedCoin('1rs')} className={`px-3 h-5 md:text-sm border-r-[1px] ${selectedCoin === '1rs' ? 'text-blue-700' : ''}`}>1 Rs Coin</button>
-      <button onClick={() => setSelectedCoin('2rs')} className={`px-3 h-5 md:text-sm border-r-[0px] ${selectedCoin === '2rs' ? 'text-blue-700' : ''}`}>2 Rs Coin</button>
+    <nav className="flex justify-center gap-4 sm:gap-10 md:gap-16 py-4 border-b-[1px] text-xs  text-[#d31c92] ">
+      <button onClick={() => setSelectedCoin('virtual')} className={`px-3 h-4 md:text-sm border-r-[0px] line-clamp-1 ${selectedCoin === 'virtual' ? 'text-blue-700' : ''}`}>Virtual Coin</button>
+      <button onClick={() => setSelectedCoin('10rs')} className={`pr-3 h-4 md:text-sm border-r-[1px] line-clamp-1  ${selectedCoin === '10rs' ? 'text-blue-700' : ''}`}>10 Rs Coin</button>
+      <button onClick={() => setSelectedCoin('5rs')} className={`pr-3 h-4 md:text-sm border-r-[1px] line-clamp-1 ${selectedCoin === '5rs' ? 'text-blue-700' : ''}`}>5 Rs Coin</button>
+      <button onClick={() => setSelectedCoin('1rs')} className={`pr-3 h-4 md:text-sm border-r-[1px] line-clamp-1 ${selectedCoin === '1rs' ? 'text-blue-700' : ''}`}>1 Rs Coin</button>
+      <button onClick={() => setSelectedCoin('2rs')} className={`pr-3 h-4 md:text-sm border-r-[0px] line-clamp-1 ${selectedCoin === '2rs' ? 'text-blue-700' : ''}`}>2 Rs Coin</button>
     </nav>
   );
 };
